893c34bf15a0aedbfb6e461e734723b41ae50445
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-03-09 10:38:01 -0600

    fix gulpfile. corrected directories. add vendor/bootstrap as includePath. error notifications (also dont break watch tasks)

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,50 @@
+var gulp         = require('gulp'),
+//var bower        = require('gulp-bower'),
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync'),
+    prefix       = require('gulp-autoprefixer'),
+    plumber      = require('gulp-plumber'),
+    uglify       = require('gulp-uglify'),
+    rename       = require("gulp-rename"),
+    imagemin     = require("gulp-imagemin"),
+    pngquant     = require('imagemin-pngquant'),
+    gutil        = require('gulp-util');
 
-var gulp         = require('gulp');
-//var bower        = require('gulp-bower');
-var sass         = require('gulp-sass');
-var browserSync  = require('browser-sync');
-var prefix       = require('gulp-autoprefixer');
-var plumber      = require('gulp-plumber');
-var uglify       = require('gulp-uglify');
-var rename       = require("gulp-rename");
-var imagemin     = require("gulp-imagemin");
-var pngquant     = require('imagemin-pngquant');
 
-
-var srcDir       = './src',
-    publicDir    = './public',
-    vendorDir    = './vendor';
+var srcDir       = 'src/',
+    publicDir    = 'public/',
+    vendorDir    = 'vendor/';
+var bowerDir     = vendorDir + 'bower_components/';
 
 
 var config = {
-    jsDir: srcDir + '/js',
-    sassDir: srcDir + '/sass',
-    bowerDir: vendorDir + '/bower_components'
+    js: {
+        src: srcDir + 'js',
+        pub: publicDir + 'js'
+    },
+    scss : {
+        src: srcDir + 'scss',
+        styleCss: srcDir + 'scss/style.scss',
+        pub: publicDir + 'css',
+        sassOpts: {
+            outputStyle: 'nested', // todo: compressed for prod
+            precison: 3,
+            errLogToConsole: true,
+            includePaths: [ bowerDir + 'bootstrap/scss']
+        }
+    }
+};
+
+
+var onError = function (error) {
+    gutil.beep();
+    gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+    this.emit('end');
 };
 
 
 /**
- jsDir: './src/js',
- *
- * Basically runs bower install
- * - execute via 'gulp bower'
- *
+ * Run bower install - execute via 'gulp bower'
  **/
 /*
 gulp.task('bower', function() {
@@ -39,69 +55,51 @@ gulp.task('bower', function() {
 
 
 /**
- *
- * Styles
- * - Compile
- * - Compress/Minify
- * - Catch errors (gulp-plumber)
- * - Autoprefixer
- *
+ * Sass - minify, autoprefix
  **/
-
-
 gulp.task('sass', function() {
-    gulp.src( config.sassDir + '/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+    return gulp.src([
+            config.scss.src + '/**/*.scss'
+            //!config.scss.src + 'bootstrap/*.scss' // ignores contents of src/bootstrap dir
+        ])
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(sass(config.scss.sassOpts))
         .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
-        .pipe(plumber())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(config.scss.pub));
 });
 
 
 
-
-
-
 /**
- *
- * BrowserSync.io
- * - Watch CSS, JS & HTML for changes
- * - View project at: localhost:3000
- *
+ * BrowserSync.io - localhost:3000
  **/
 gulp.task('browser-sync', function() {
-    browserSync.init([publicDir + '/css/*.css', publicDir + '/js/**/*.js', 'index.html'], {
+    browserSync.init([publicDir + 'css/*.css', publicDir + 'js/**/*.js', 'index.html'], {
         server: {
-            baseDir: './'
+            baseDir: publicDir
         }
     });
 });
 
 
 /**
- *
- * Javascript
- * - Uglify
- *
+ * JS - Uglify
  **/
 gulp.task('scripts', function() {
     gulp.src(publicDir + 'js/*.js')
         .pipe(uglify())
         .pipe(rename({
             dirname: "min",
-            suffix: ".min",
+            suffix: ".min"
         }))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest(publicDir + 'js'))
 });
 
 /**
- *
- * Images
- * - Compress them!
- *
+ * Compress images
  **/
 gulp.task('images', function () {
-    return gulp.src('images/*')
+    return gulp.src(publicDir + 'images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
@@ -111,17 +109,13 @@ gulp.task('images', function () {
 });
 
 
-/**
- *
- * Default task
- * - Runs sass, browser-sync, scripts and image tasks
- * - Watchs for file changes for images, scripts and sass/css
- *
- **/
-gulp.task('default', ['sass', 'browser-sync', 'scripts', 'images']);
 
-gulp.task('watch', ['sass', 'browser-sync', 'scripts', 'images'], function () {
-    gulp.watch(config.sassDir + '/**/*.scss', ['sass']);
-    gulp.watch(config.jsDir + 'js/**/*.js', ['scripts']);
-    gulp.watch('images/*', ['images']);
+
+
+gulp.task('default', ['sass', /*'browser-sync',*/ 'scripts', 'images', 'watch']);
+
+gulp.task('watch', function () {
+    gulp.watch(config.scss.src + '**/*.scss', ['sass']);
+    gulp.watch(config.js.src + '**/*.js', ['scripts']);
+    gulp.watch(publicDir + 'images/*', ['images']);
 });
