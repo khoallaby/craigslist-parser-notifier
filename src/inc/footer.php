9a05aa28866c02a7864ec06969b5740acb720b07
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-03 23:12:08 -0600

    hide execution time debug info in footer

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -2,10 +2,12 @@
         <section><?php
             $timeStart = \Craigslist\WebUI::$timeStart;
             $time = microtime(true) - $timeStart;
+            /*
             echo sprintf( 'Execution time: <b>%s</b> | %s posts found',
                 $time,
                 Craigslist\Database::getInstance()->getMysqliDb()->totalCount
             );
+            */
         ?></section>
     </footer>
 </body>
